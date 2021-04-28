ce0f630cf8dc2f4b704813e8239670713a1c2994
dfayika1988 <47186916+dfayika1988@users.noreply.github.com>
2021-04-28 10:37:09 +0100

    Create currency.cpp
    currency.cpp implementation file

---
+++ currency.cpp
@@ -0,0 +1,16 @@
+#include "currency.h"
+
+Currency::currency(const string& short_name, const string& name)
+{
+  m_short_name = short_name
+}
+
+const string& Currency::get_short_name() const
+{
+  return m_short_name;
+}
+
+const string& Currency::get_name() const
+{
+  return n_name;
+}
