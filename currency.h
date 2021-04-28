0e26972ef60a9ffa7bd9ca4c49c8526a77d9d2a4
dfayika1988 <47186916+dfayika1988@users.noreply.github.com>
2021-04-28 19:41:25 +0100

    Update currency.h
    int C_numericode

--- currency.h
+++ currency.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -9,14 +10,19 @@ class Currency
 {
   string c_name;
   string c_code;
+  int c_numericCode;
 
   
 public:
   Currency(const string& name, const string& code ):
-         c_name(name), c_code(code)  {}
+         c_name(name), c_code(code) {}
   ~Currency() = default;
+    
+    //, c_numericCode (numberCode)
   
   const string& get_c_name() const;
   const string& get_code() const;
-
-};
+  const int get_numericCode () const;
+  void get_c_nsme (const string & value);
+  void get_code (const string & value);
+  void get_numericCode (const int value);
